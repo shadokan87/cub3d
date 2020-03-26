4a0482a2707b618d9e2ea13e00583f2fae05914c
Eclipse <eclipse@Eclipses-iMac.local>
2020-03-26 18:25:24 +0000

    save

--- main2.c
+++ main2.c
@@ -3,9 +3,44 @@
 #include <string.h>
 #include <mlx.h>
 #include <math.h>
+#include <unistd.h>
 
 #define mapWidth 24
 #define mapHeight 24
+
+void	ft_putstr(char *str)
+{
+	int i;
+
+	i = -1;
+	while (str[++i]);
+	write(1, str, i);
+}
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	int neg;
+
+	neg = 0;
+	neg = nb < 0 ? 1 : 0;
+	neg ? ft_putchar('-') : 0;
+	if (nb == -2147483648)
+		ft_putstr("2147483648");
+	if (nb == 0)
+		ft_putchar('0');
+	if (nb == 0 || nb == -2147483648)
+		return ;
+	nb < 0 ? (nb = -nb) : 0;
+	if (nb == 0)
+		ft_putchar(0);
+	nb > 9 ? ft_putnbr(nb / 10) : 0;
+	ft_putchar(nb % 10 + '0');
+}
 
 typedef struct    data_s
 {
@@ -16,20 +51,7 @@ typedef struct    data_s
     
 }                 data_t;
 
-typedef struct	pixel_put_s
-{
-	void	*img;
-	int	endian;
-	int	bpp;
-	int	line;
-	int	offset;
-	int	x;
-	int	s_h;
-	int	s_w;
-	char	*addr;
-}	pixel_info_t;
-
-typedef struct double_var_s
+typedef struct var_s
 {
 	double posX;
 	double posY;
@@ -47,11 +69,6 @@ typedef struct double_var_s
 	double deltaDistX;
 	double deltaDistY;
 	double perpWallDist;
-
-}	dbl_t;
-
-typedef struct integer_var_s
-{
 	int mapX;
 	int mapY;
 	int stepX;
@@ -62,17 +79,31 @@ typedef struct integer_var_s
 	int drawStart;
 	int drawEnd;
 	int color;
-}	int_t;
+	int		ESC;
+	int		W;
+	int		A;
+	int		S;
+	int		D;
+	void	*img;
+	int	endian;
+	int	bpp;
+	int	line;
+	int	offset;
+	int	x;
+	int	s_h;
+	int	s_w;
+	char	*addr;
+}	var_t;
 
-void	pixel_put(pixel_info_t *pixel, int x, int y, int color)
+void	pixel_put(var_t *var, int x, int y, int color)
 {
 	char *dst;
 
-	dst = pixel->addr + (y * pixel->line + x * (pixel->bpp / 8));
+	dst = var->addr + (y * var->line + x * (var->bpp / 8));
 	*(unsigned int*)dst = color;
 }
 
-int	verline(pixel_info_t *pixel, int x, int draw_start, int draw_end, int color)
+int	verline(var_t *var, int x, int draw_start, int draw_end, int color)
 {
 	int swap;
 
@@ -82,121 +113,211 @@ int	verline(pixel_info_t *pixel, int x, int draw_start, int draw_end, int color)
 		draw_start = draw_end;
 		draw_end = swap;
 	}
-	if (draw_end < 0 || draw_start >= pixel->s_h || x < 0 || x >= pixel->s_w)
+	if (draw_end < 0 || draw_start >= var->s_h || x < 0 || x >= var->s_w)
 		return (0);
 	if (draw_start < 0)
 		draw_start = 0;
-	if (draw_end >= pixel->s_w)
-		draw_end = pixel->s_h - 1;
+	if (draw_end >= var->s_w)
+		draw_end = var->s_h - 1;
 	while (draw_start < draw_end)
 	{
-		pixel_put(pixel, x, draw_start, color);
+		pixel_put(var, x, draw_start, color);
 		draw_start++;
 	}
 	return (1);
 }
 
-void	init_raycast(pixel_info_t *pixel, data_t *data, dbl_t *dbl)
+void	init_raycast(data_t *data, var_t *var)
 {
-	pixel->s_w = data->s_w;
-	pixel->s_h = data->s_h;
-	pixel->img = mlx_new_image(data->mlx_ptr, data->s_w, data->s_h);
-	pixel->addr = mlx_get_data_addr(pixel->img, &pixel->bpp, &pixel->line, &pixel->endian);
-	dbl->posX = 22;
-	dbl->posY = 12;
-	dbl->dirX = -1;
-	dbl->dirY = 0;
-	dbl->planeX = 0;
-	dbl->planeY = 0.66;
-	dbl->time = 0;
-	dbl->oldTime = 0;
+	var->s_w = data->s_w;
+	var->s_h = data->s_h;
+	var->img = mlx_new_image(data->mlx_ptr, data->s_w, data->s_h);
+	var->addr = mlx_get_data_addr(var->img, &var->bpp, &var->line, &var->endian);
+	var->posX = 22;
+	var->posY = 12;
+	var->dirX = -1;
+	var->dirY = 0;
+	var->planeX = 0;
+	var->planeY = 0.66;
+	var->time = 0;
 
 }
 
-void	step(dbl_t *dbl, int_t *integ)
+void	step(var_t *var)
 {
-	if (dbl->rayDirX < 0)
+	if (var->rayDirX < 0)
 	{
-		integ->stepX = -1;
-		dbl->sideDistX = (dbl->posX - integ->mapX) * dbl->deltaDistX;
+		var->stepX = -1;
+		var->sideDistX = (var->posX - var->mapX) * var->deltaDistX;
 	}
 	else
 	{
-		integ->stepX = 1;
-		dbl->sideDistX = (integ->mapX + 1.0 - dbl->posX) * dbl->deltaDistX;
+		var->stepX = 1;
+		var->sideDistX = (var->mapX + 1.0 - var->posX) * var->deltaDistX;
 	}
-	if (dbl->rayDirY < 0)
+	if (var->rayDirY < 0)
 	{
-		integ->stepY = -1;
-		dbl->sideDistY = (dbl->posY - integ->mapY) * dbl->deltaDistY;
+		var->stepY = -1;
+		var->sideDistY = (var->posY - var->mapY) * var->deltaDistY;
 	}
 	else
 	{
-		integ->stepY = 1;
-		dbl->sideDistY = (integ->mapY + 1.0 - dbl->posY) * dbl->deltaDistY;
+		var->stepY = 1;
+		var->sideDistY = (var->mapY + 1.0 - var->posY) * var->deltaDistY;
 	}
 }
 
-void	hit(int worldMap[mapWidth][mapHeight], dbl_t *dbl, int_t *integ)
+void	hit(int worldMap[mapWidth][mapHeight], var_t *var)
 {
-	while (integ->hit == 0)
+	while (var->hit == 0)
 	{
-		if (dbl->sideDistX < dbl->sideDistY)
+		if (var->sideDistX < var->sideDistY)
 		{
-			dbl->sideDistX += dbl->deltaDistX;
-			integ->mapX += integ->stepX;
-			integ->side = 0;
+			var->sideDistX += var->deltaDistX;
+			var->mapX += var->stepX;
+			var->side = 0;
 		}
 		else
 		{
-			dbl->sideDistY += dbl->deltaDistY;
-			integ->mapY += integ->stepY;
-			integ->side = 1;
+			var->sideDistY += var->deltaDistY;
+			var->mapY += var->stepY;
+			var->side = 1;
 		}
-		if (worldMap[integ->mapX][integ->mapY] > 0)
-			integ->hit = 1;
+		if (worldMap[var->mapX][var->mapY] > 0)
+			var->hit = 1;
 	}
-	if (integ->side == 0)
-		dbl->perpWallDist = (integ->mapX - dbl->posX + (1 - 
-		integ->stepX) / 2) / dbl->rayDirX;
+	if (var->side == 0)
+		var->perpWallDist = (var->mapX - var->posX + (1 - 
+		var->stepX) / 2) / var->rayDirX;
 	else
-		dbl->perpWallDist = (integ->mapY - dbl->posY + (1 - integ->stepY) / 2) / dbl->rayDirY;	
+		var->perpWallDist = (var->mapY - var->posY + (1 - var->stepY) / 2) / var->rayDirY;	
 }
 
-void	draw(data_t *data, dbl_t *dbl, int_t *integ, pixel_info_t *pixel)
+void	draw(var_t *var, data_t *data, var_info_t *var)
 {
-	integ->lineHeight = (int)(data->s_h / dbl->perpWallDist);
-	integ->drawStart = -integ->lineHeight / 2 + data->s_h / 2;
-	if (integ->drawStart < 0)
-		integ->drawStart = 0;
-	integ->drawEnd = integ->lineHeight / 2 + data->s_h / 2;
-	if (integ->drawEnd >= data->s_h)
-		integ->drawEnd = data->s_h - 1;
-	integ->color = 255;
-	if (integ->side == 1)
-		integ->color = integ->color / 2;
-	verline(pixel, pixel->x, integ->drawStart, integ->drawEnd, integ->color);
+	var->lineHeight = (int)(data->s_h / var->perpWallDist);
+	var->drawStart = -var->lineHeight / 2 + data->s_h / 2;
+	if (var->drawStart < 0)
+		var->drawStart = 0;
+	var->drawEnd = var->lineHeight / 2 + data->s_h / 2;
+	if (var->drawEnd >= data->s_h)
+		var->drawEnd = data->s_h - 1;
+	var->color = 255;
+	if (var->side == 1)
+		var->color = var->color / 2;
+	verline(var, var->x, var->drawStart, var->drawEnd, var->color);
 }
 
-void	raycast(pixel_info_t *pixel, data_t *data, dbl_t *dbl, int_t *integ,
+void	raycast(var_info_t *var, data_t *data, var_t *var,
 int worldMap[mapWidth][mapHeight])
 {
-	pixel->x = 0;
-	while (pixel->x < data->s_w)
+	var->x = 0;
+	while (var->x < data->s_w)
 	{
-		dbl->cameraX = 2 * pixel->x / (double)data->s_w - 1;
-		dbl->rayDirX = dbl->dirX + dbl->planeX * dbl->cameraX;
-		dbl->rayDirY = dbl->dirY + dbl->planeY * dbl->cameraX;
-		integ->mapX = (int)dbl->posX;
-		integ->mapY = (int)dbl->posY;
-		dbl->deltaDistX = fabs(1 / dbl->rayDirX);
-		dbl->deltaDistY = fabs(1 / dbl->rayDirY);
-		integ->hit = 0;
-		step(dbl, integ);
-		hit(worldMap, dbl, integ);
-		draw(data, dbl, integ, pixel);
-		pixel->x++;
+		var->cameraX = 2 * var->x / (double)data->s_w - 1;
+		var->rayDirX = var->dirX + var->planeX * var->cameraX;
+		var->rayDirY = var->dirY + var->planeY * var->cameraX;
+		var->mapX = (int)var->posX;
+		var->mapY = (int)var->posY;
+		var->deltaDistX = fabs(1 / var->rayDirX);
+		var->deltaDistY = fabs(1 / var->rayDirY);
+		var->hit = 0;
+		step(var);
+		hit(worldMap, var);
+		draw(var, data, var);
+		var->x++;
 	}
+}
+
+void	init_keys(var_t *var, int key_number)
+{
+	int *ptr;
+
+	ptr = &var->ESC;
+	while (key_number)
+	{
+		*ptr = 0;
+		ptr++;
+		key_number--;
+	}
+}
+
+int		key_pressed(int keycode, var_t *var)
+{
+	if (keycode == 53)
+	{
+		var->ESC = 1;
+		ft_putstr("\nESC = ");
+		ft_putnbr(var->ESC);
+	}
+		
+	else if (keycode == 13)
+	{
+		var->W = 1;
+		ft_putstr("\nW = ");
+		ft_putnbr(var->W);
+	}
+		
+	else if (keycode == 0)
+	{
+		var->A = 1;
+		ft_putstr("\nA = ");
+		ft_putnbr(var->A);
+	}
+		
+	else if (keycode == 1)
+	{
+		var->S = 1;
+		ft_putstr("\nS = ");
+		ft_putnbr(var->S);
+	}
+		
+	else if (keycode == 2)
+	{
+		var->D = 1;
+		ft_putstr("\nD = ");
+		ft_putnbr(var->D);
+	}
+	return (0);	
+}
+
+int		key_released(int keycode, var_t *var)
+{
+	if (keycode == 53)
+	{
+		var->ESC = 0;
+		ft_putstr("\nESC = ");
+		ft_putnbr(var->ESC);
+	}
+		
+	else if (keycode == 13)
+	{
+		var->W = 0;
+		ft_putstr("\nW = ");
+		ft_putnbr(var->W);
+	}
+		
+	else if (keycode == 0)
+	{
+		var->A = 0;
+		ft_putstr("\nA = ");
+		ft_putnbr(var->A);
+	}
+		
+	else if (keycode == 1)
+	{
+		var->S = 0;
+		ft_putstr("\nS = ");
+		ft_putnbr(var->S);
+	}
+		
+	else if (keycode == 2)
+	{
+		var->D = 0;
+		ft_putstr("\nD = ");
+		ft_putnbr(var->D);
+	}
+	return (0);
 }
 
 int	main(void)
@@ -229,9 +350,7 @@ int	main(void)
   {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
 };
 	data_t data;
-	pixel_info_t pixel;
-	dbl_t dbl;
-	int_t integ;
+	var_t var;
 
 	data.s_w = 640;
 	data.s_h = 480;
@@ -239,9 +358,13 @@ int	main(void)
 		return (EXIT_FAILURE);
 	if ((data.mlx_win = mlx_new_window(data.mlx_ptr, data.s_w, data.s_h, "cub3d")) == NULL)
 	return (EXIT_FAILURE);
-	init_raycast(&pixel, &data, &dbl);
-	raycast(&pixel, &data, &dbl, &integ, worldMap);
-	mlx_put_image_to_window(data.mlx_ptr, data.mlx_win, pixel.img, 0, 0);
+	init_raycast(&var, &data, &var);
+	raycast(&var, &data, &var, worldMap);
+	mlx_put_image_to_window(data.mlx_ptr, data.mlx_win, var.img, 0, 0);
+	init_keys(&var, 5);
+	mlx_hook(data.mlx_win, 2, 2, key_pressed, &var);
+	mlx_key_hook(data.mlx_win, key_released, &var);
 	mlx_loop(data.mlx_ptr);
 	return (EXIT_SUCCESS);
 }
+
