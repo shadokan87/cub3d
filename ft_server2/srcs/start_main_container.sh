4635db4f4c9890dc7e5f192ec0d093eb1e24ba5b
shadokan87 <shadokan87@shadokan87s-Mac.local>
2020-07-01 10:19:15 -0700

    ft_server

--- ft_server2/srcs/start_main_container.sh
+++ ft_server2/srcs/start_main_container.sh
@@ -4,5 +4,6 @@ service mysql stop
 service mysql start
 cp /etc/hosts /etc/hosts2
 sed -i '1 s/localhost/www.ftserver.com/' /etc/hosts2
+echo "127.0.0.1		ftserver.com" >> /etc/hosts2
 cat /etc/hosts2 > /etc/hosts
 tail -f /dev/null
