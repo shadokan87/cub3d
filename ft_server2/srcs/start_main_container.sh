a640b491dc277ef4015c5a5009207a36dc46f616
shadokan87 <shadokan87@shadokan87s-Mac.local>
2020-07-07 17:49:57 -0700

    ft_server

--- ft_server2/srcs/start_main_container.sh
+++ ft_server2/srcs/start_main_container.sh
@@ -1,9 +1,10 @@
+cp /etc/hosts /etc/hosts2
+sed -i '1 s/localhost/www.ftserver.com/' /etc/hosts2
+echo "127.0.0.1		https://ftserver.com" >> /etc/hosts2
+echo "ftserver.com		https://ftserver.com" >> /etc/hosts2
+cat /etc/hosts2 > /etc/hosts
 service nginx start
 service php7.3-fpm start
 service mysql stop
 service mysql start
-cp /etc/hosts /etc/hosts2
-sed -i '1 s/localhost/www.ftserver.com/' /etc/hosts2
-echo "127.0.0.1		ftserver.com" >> /etc/hosts2
-cat /etc/hosts2 > /etc/hosts
 tail -f /dev/null
